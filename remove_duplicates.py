7628162b0a18ea5802c4629464ce81a7790b4f3a
RAJAT KATIYAR <rajatkatiyar2009@gmail.com>
2019-03-29 23:26:59 +0530

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,8 +1,15 @@
 """Write a program (function!) that takes a list and returns a new list that
-contains all the elements of the first list minus all the duplicates."""
+contains all the elements of the first list minus all the duplicates.
+Write two different functions to do this - one using a loop and constructing a list, and another using sets.
+"""
+
+def remove_duplicates_using_set(list):
+    set1 = set(list)
+    new_list = [i for i in set1]
+
+    return new_list
 
 list = [int(i) for i in input("Please input the list(elements separated by commas(,))\n").split(',')]
-set1 = set(list)
-new_list = [i for i in set1]
 
-print(new_list)
+print("The output using sets ",remove_duplicates_using_set(list))
+# print("The output using loops ",remove_duplicates_using_loop(list))
