7cc0f2ac3db84b0c7f4a7ef4063bbf067517cfdc
RAJAT KATIYAR <rajatkatiyar2009@gmail.com>
2019-03-29 23:28:15 +0530

    added another fn

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -3,6 +3,13 @@ contains all the elements of the first list minus all the duplicates.
 Write two different functions to do this - one using a loop and constructing a list, and another using sets.
 """
 
+def remove_duplicates_using_loop(list):
+    new_list = []
+    for i in range(len(list)):
+        if list[i] not in new_list:
+            new_list.append(list[i])
+    return new_list
+
 def remove_duplicates_using_set(list):
     set1 = set(list)
     new_list = [i for i in set1]
@@ -12,4 +19,4 @@ def remove_duplicates_using_set(list):
 list = [int(i) for i in input("Please input the list(elements separated by commas(,))\n").split(',')]
 
 print("The output using sets ",remove_duplicates_using_set(list))
-# print("The output using loops ",remove_duplicates_using_loop(list))
+print("The output using loops ",remove_duplicates_using_loop(list))
